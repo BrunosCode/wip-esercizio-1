81963dec11fab001486537fb0abeca1ae6fbaba4
brunEdo <edobruno.m@gmail.com>
2022-02-02 14:09:37 +0100

    filters bar

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import "./style.scss";
+
+const filters = document.getElementById("filters");
+const openFilters = document.getElementById("open-filters");
+
+openFilters.addEventListener("click", () => {
+  // togle the dataset boolean value 
+  filters.dataset.open = !(filters.dataset.open === "true");
+})
